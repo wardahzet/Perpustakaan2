edab93595a1bd0b60d33014de68e85188d214f47
Siti Waheeda Najihah <sitiwaheeda14@student.ub.ac.id>
2023-03-20 17:02:02 +0700

    Update PeminjamanController.java

--- Perpustakaan/src/perpustakaan/PeminjamanController.java
+++ Perpustakaan/src/perpustakaan/PeminjamanController.java
@@ -1,5 +1,6 @@
 package perpustakaan;
 
+import java.awt.Dialog;
 import java.util.ArrayList;
 
 public class PeminjamanController {
@@ -9,8 +10,24 @@ public class PeminjamanController {
         Perpustakaan.formPeminjaman.tampilkan();
     }
 
-    public void cariBuku(String buku) {
-        
+    public void cariBuku(String judul) {
+        BukuProvider bukuProvider = new BukuProvider();
+        try {
+            ArrayList<Buku> listBuku = bukuProvider.selectBuku(judul);
+            if(listBuku.isEmpty())
+            {
+                DialogUI dialogUI = new DialogUI("Buku tidak terdaftar");
+                dialogUI.pack();
+                dialogUI.setLocationRelativeTo(null);
+                dialogUI.setVisible(true);
+            } 
+            else Perpustakaan.formPeminjaman.display(listBuku);
+        } catch(Exception ex) {
+            DialogUI dialogUI = new DialogUI("Connection Error");
+            dialogUI.pack();
+            dialogUI.setLocationRelativeTo(null);
+            dialogUI.setVisible(true);
+        }
     }
 
     @SuppressWarnings("unchecked")
